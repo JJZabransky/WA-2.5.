83a61f5192eb1c450c10669d437ab9883fde0293
Jan Jakub Zábranský <zabransky@SPSEJECNANET.CZ>
2023-05-02 08:00:56 +0200

    second commit

---
+++ redirect.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+
+    case '':
+    case '/':
+        $redirect = 'index.php';
+        break;
+
+    case '/admin':
+        $redirect = 'admin.php';
+        break;
+
+    case '/main':
+        $redirect = 'main.php';
+        break;
+
+    default:
+        http_response_code(404);
+        require __DIR__ . 'error.php';
+        exit();
+}
+
+$_SESSION['site'] = $redirect;
+require_once __DIR__ . 'header.php';
+require_once __DIR__ . $redirect ?? __DIR__ . 'index.php';
+require_once __DIR__ . 'footer.php';
